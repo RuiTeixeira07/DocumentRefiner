60ae8f0f1e3e99a11f10a092b56805eed2c11474
Rui Teixeira <al2025155352@alunos.utad.pt>
2026-05-08 23:07:56 +0100

    Add 'Pagination' Handling.

--- DocumentRefiner/refiner/refiner.py
+++ DocumentRefiner/refiner/refiner.py
@@ -1,7 +1,11 @@
+import re
 from DocumentRefiner.model.document import Document
 from DocumentRefiner.model.refined_document import RefinedDocument
 from DocumentRefiner.resources.extensions.string import StringExtensions
 from DocumentRefiner.resources.format.format import Format
+from DocumentRefiner.resources.model.line_ending import CARRIAGE_RETURN, LINE_FEED, END_OF_LINE
+
+pagination = fr"(\d)+/(\d)+({CARRIAGE_RETURN.value}|{LINE_FEED.value}|{END_OF_LINE.value})"
 
 class Refiner:
     @staticmethod
@@ -11,14 +15,16 @@ class Refiner:
         remove_blank_lines: bool = True,
         remove_consecutive_paragraphs: bool = True,
         remove_leading_artifacts: bool = True,
-        remove_trailing_artifacts: bool = True) -> RefinedDocument:
+        remove_trailing_artifacts: bool = True,
+        remove_pagination: bool = True) -> RefinedDocument:
 
         if (remove_consecutive_whitespace_characters
             and remove_blank_lines
             and remove_consecutive_paragraphs
             and remove_leading_artifacts
-            and remove_trailing_artifacts):
-            return RefinedDocument(document, Format.clean_text(document.data))
+            and remove_trailing_artifacts
+            and remove_pagination):
+            return RefinedDocument(document, Refiner.remove_pagination(Format.clean_text(document.data)))
 
         data = document.data
 
@@ -37,4 +43,11 @@ class Refiner:
         if remove_trailing_artifacts:
             data = StringExtensions.remove_trailing_artifacts(data)
 
+        if remove_pagination:
+            data = Refiner.remove_pagination(data)
+
         return RefinedDocument(document, data)
+
+    @staticmethod
+    def remove_pagination(text: str) -> str:
+        return re.sub(pagination, "", text)
